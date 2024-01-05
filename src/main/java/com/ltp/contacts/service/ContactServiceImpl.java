6b215f79e01a22db2d949737e9a3d0a7d1475c26
nuthapon <nuthaponm79@gmail.com>
2024-01-05 22:44:47 +0700

    feat: exception not found

--- src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -4,14 +4,13 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.exception.NoContactException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
-@ConditionalOnProperty(name = "server.port", havingValue = "8080")
 public class ContactServiceImpl implements ContactService {
 
     @Autowired
@@ -27,7 +26,7 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Override
-    public Contact getContactById(String id) {
+    public Contact getContactById(String id) throws NoContactException {
         return contactRepository.getContact(findIndexById(id));
     }
 
@@ -37,20 +36,20 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Override
-    public void updateContact(String id, Contact contact) {
+    public void updateContact(String id, Contact contact) throws NoContactException {
         contactRepository.updateContact(findIndexById(id), contact);
     }
 
     @Override
-    public void deleteContact(String id) {
+    public void deleteContact(String id) throws NoContactException {
         contactRepository.deleteContact(findIndexById(id));
     }
     
-    private int findIndexById(String id) {
+    private int findIndexById(String id) throws NoContactException {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(() -> new NoContactException());
     }
 
 }
