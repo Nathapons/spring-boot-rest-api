765d4ac44a3695a472cd7113f2ff20f4d1ba4efe
nuthapon <nuthaponm79@gmail.com>
2024-01-05 23:28:10 +0700

    feat: Field Validation

--- src/main/java/com/ltp/contacts/exception/ErrorResponse.java
+++ src/main/java/com/ltp/contacts/exception/ErrorResponse.java
@@ -1,27 +1,19 @@
 package com.ltp.contacts.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class ErrorResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
-    private String message;
+    private List<String> message;
 
-    public ErrorResponse(String message) {
+    public ErrorResponse(List<String> message) {
+        this.message = message;
         this.timestamp = LocalDateTime.now();
-        this.message = message;
     }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
 
     public LocalDateTime getTimestamp() {
         return this.timestamp;
@@ -29,6 +21,14 @@ public class ErrorResponse {
 
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public List<String> getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
     }
 
 }
