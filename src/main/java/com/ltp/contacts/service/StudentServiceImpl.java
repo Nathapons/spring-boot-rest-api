c5b436a8c413f7026b889dd5ce7fc695706c4278
nuthapon <nuthaponm79@gmail.com>
2024-01-10 15:37:12 +0700

    remve: unuse code

--- src/main/java/com/ltp/contacts/service/StudentServiceImpl.java
+++ src/main/java/com/ltp/contacts/service/StudentServiceImpl.java
@@ -3,7 +3,6 @@ package com.ltp.contacts.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ltp.contacts.entity.Student;
