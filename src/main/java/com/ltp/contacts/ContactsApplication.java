4edbecdad6a0f87f4b6acdb9efbe7437057443ea
nuthapon <nuthaponm79@gmail.com>
2024-01-10 17:53:03 +0700

    feat: initial course datas

--- src/main/java/com/ltp/contacts/ContactsApplication.java
+++ src/main/java/com/ltp/contacts/ContactsApplication.java
@@ -2,19 +2,23 @@ package com.ltp.contacts;
 
 import java.time.LocalDate;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ltp.contacts.entity.Course;
 import com.ltp.contacts.entity.Student;
+import com.ltp.contacts.repository.CourseRepository;
 import com.ltp.contacts.repository.StudentRepository;
 
+import lombok.AllArgsConstructor;
+
 @SpringBootApplication
+@AllArgsConstructor
 public class ContactsApplication implements CommandLineRunner{
 
-	@Autowired
 	StudentRepository studentRepository;
+	CourseRepository courseRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApplication.class, args);
@@ -31,6 +35,19 @@ public class ContactsApplication implements CommandLineRunner{
 
 		for (int i = 0; i < students.length; i ++) {
 			studentRepository.save(students[i]);
+		}
+
+		Course[] courses = new Course[] {
+            new Course("Charms", "CH104", "In this class, you will learn spells concerned with giving an object new and unexpected properties."),
+            new Course("Defence Against the Dark Arts", "DADA", "In this class, you will learn defensive techniques against the dark arts."),
+            new Course("Herbology", "HB311", "In this class, you will learn the study of magical plants and how to take care of, utilise and combat them."),
+            new Course("History of Magic", "HIS393", "In this class, you will learn about significant events in wizarding history."),
+            new Course("Potions", "POT102", "In this class, you will learn correct mixing and stirring of ingredients to create mixtures with magical effects."),
+            new Course("Transfiguration", "TR442", "In this class, you will learn the art of changing the form or appearance of an object.")
+        };
+
+		for (Course course : courses) {
+			courseRepository.save(course);
 		}
 	}
 
